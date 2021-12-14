3839a74aaff764e97b2a284bab77a1cdecffb12b
lupita alvarez <alv.lu1305@gmail.com>
2021-12-13 20:57:34 -0800

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -11,7 +11,7 @@ function restricted(req, res, next) {
   if (req.session.user) {
     next()
   } else {
-    next({ status: 401, message: 'no pass'})
+    next({ status: 401, message: 'you shall not pass'})
   }
 }
 
